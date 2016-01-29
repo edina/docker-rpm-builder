ab1411320e651ab7b391462b80a1fbe8b66330a5
Andrew Seales <andrew.seales@ed.ac.uk>
2016-01-29 09:25:08 +0000

    Initial commit.
    Add GDAL package.

---
+++ README.md
@@ -0,0 +1,13 @@
+Building RPMs Using Docker
+==========================
+
+This is a collection of scripts used to build various RPMs for CentOS6/RHEL6 using docker.
+
+## Usage
+
+```
+cd <package>
+./run.sh
+```
+
+The generated RPMs will appear in the package `output` folder.
---
+++ gdal/fpm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+fpm -v 1.11.3 \
+    --iteration 3.el6 \
+    --epoch 1 \
+    --vendor EDINA \
+    --provides gdal \
+    -d postgresql \
+    -d libpng \
+    -d libjpeg \
+    -d giflib \
+    -d xerces-c \
+    -d curl \
+    -d sqlite \
+    -d geos \
+    -d expat \
+    -d libxml2 \
+    -d proj-epsg \
+    -d proj-devel \
+    -a x86_64 \
+    -m andrew.seales@ed.ac.uk \
+    -n gdal \
+    -s dir \
+    -C /tmp/gdal \
+    -t rpm \
+    usr
