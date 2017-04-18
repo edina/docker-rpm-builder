26378c61ecf313515c41136f12639b361b99f192
John Pinto <john.pinto@ed.ac.uk>
2017-04-18 16:38:36 +0100

    lastools-el7: Instead of hard-coding the LASTools version in the fpm.sh, we now capture the version by running "laszip -version".

--- lastools-el7/fpm.sh
+++ lastools-el7/fpm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-version=20170414
+# The version can be captured by running a LAStools binary, e.g.,
+# > laszip -version
+# The output we get is of the form:
+# "LAStools (by martin@rapidlasso.com) version 170414"
+# For some unknown reason, the output is redirected to the stderr (file descriptor 2).
+# So we capture it as follows.s
+version_output=$(/root/LAStools/bin/laszip -version 2>&1)
+# The version is last in the text, so we reverse the text, get the reversed version substring,
+# and then reverse this again.
+version=$(echo $version_output | rev | cut -d' ' -f1 | rev)
+echo "LasTools version: $version"
 
 fpm -v ${version} \
     --iteration 1.el7 \
