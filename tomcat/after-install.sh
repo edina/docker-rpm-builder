dc267c692d11135d00a7ebd5f1f2ee87cb3680cf
Andrew Seales <andrew.seales@ed.ac.uk>
2019-11-21 13:31:47 +0000

    Make sure tomcat .sh files are executable by anyone.

--- tomcat/after-install.sh
+++ tomcat/after-install.sh
@@ -2,5 +2,6 @@ systemctl daemon-reload
 
 chown -R tomcat:tomcat /opt/tomcat/
 chmod o+r /opt/tomcat/conf/*
-chmod o+r /opt/tomcat/bin/*
+chmod 644 /opt/tomcat/bin/*
+chmod 755 /opt/tomcat/bin/*.sh
 chmod 755 /opt/tomcat/lib/*
